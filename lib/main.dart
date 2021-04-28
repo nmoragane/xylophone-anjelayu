b4a638b1508498f942885eed6ee0378eb08d3bbb
nmoragane <nmoragane@gmail.com>
2021-04-28 22:54:11 +0530

    add buttons and audio plugin

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
@@ -8,7 +9,58 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            children: [
+              FlatButton(
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note1.wav');
+                },
+                color: Colors.red,
+                child: Text('Click Me'),
+              ),
+              FlatButton(
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note2.wav');
+                },
+                color: Colors.orange,
+                child: Text('Click Me'),
+              ),
+              FlatButton(
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note3.wav');
+                },
+                color: Colors.yellow,
+                child: Text('Click Me'),
+              ),
+              FlatButton(
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note4.wav');
+                },
+                color: Colors.green,
+                child: Text('Click Me'),
+              ),
+              FlatButton(
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note5.wav');
+                },
+                color: Colors.blue,
+                child: Text('Click Me'),
+              ),
+              FlatButton(
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play('note6.wav');
+                },
+                color: Colors.purple,
+                child: Text('Click Me'),
+              ),
+            ],
+          ),
         ),
       ),
     );
